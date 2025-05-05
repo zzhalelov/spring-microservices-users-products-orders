808e5ae1bbf082c9b6bbd3ad33f69e54c75533b2
Zhastilek Zhalelov <zzhalelov@gmail.com>
2025-05-05 23:47:41 +0500

    git init

---
+++ src/test/java/org/example/springmicroservicesusersproductsorders/SpringMicroservicesUsersProductsOrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springmicroservicesusersproductsorders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroservicesUsersProductsOrdersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
